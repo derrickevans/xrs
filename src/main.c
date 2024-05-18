cdd73d533c693347346b286b9bf4957c554d605c
Derrick Evans <x1xderrick@gmail.com>
2024-05-18 11:25:12 -0500

    Added tick update and character movement on click

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "raylib.h"
+#include "raymath.h"
 
+#include "xrs_utils.h"
 #include "xrs_character.h"
+#include "xrs_map.h"
 
 const int screen_width = 640;
 const int screen_height = 640;
@@ -9,26 +12,26 @@ Camera2D camera;
 
 xrs_character_t player;
 
-#define MAP_WIDTH 20
-#define MAP_HEIGHT 20
-#define CELL_WIDTH 32
-#define CELL_HEIGHT 32
-
 Texture2D map[MAP_WIDTH * MAP_HEIGHT];
 Texture2D grass, dirt;
+
+Vector2 mouse_pos;
+Vector2 mouse_world;
 
 void xrs_init(void) {
     InitWindow(screen_width, screen_height, "eXpress RS");
     SetTargetFPS(60);
 
-    player.pos = (Vector2) {9.0f * CELL_WIDTH, 9.0f * CELL_HEIGHT};
+    player.pos = xrs_cell_to_pixel(1, 1);
     player.sprite = LoadTexture("res/textures/ordinary_orvil.png");
     grass = LoadTexture("res/textures/grass_0001.png");
     dirt = LoadTexture("res/textures/dirt_0001.png");
 
-    camera.offset = (Vector2) {screen_width / 2, screen_height / 2};
+    //camera.offset = (Vector2) {screen_width / 2, screen_height / 2};
+    camera.offset = (Vector2) {0.0f, 0.0f};
     camera.rotation = 0.0f;
-    camera.target = player.pos;
+    //camera.target = player.pos;
+    camera.target = (Vector2) {0.0f, 0.0f};
     camera.zoom = 1.00f;
 
     // Generate the map.
@@ -44,17 +47,35 @@ void xrs_init(void) {
         }
     }
 }
-           
+
+Vector2 mouse_cell_clicked;
+Vector2 player_cell;
+Vector2 direction;
+float timer = 0.0f;
+int speed = 1;
+bool is_walking = false;
 void xrs_tick_update(void) {
+    timer += 1.0f;
+
+    if (is_walking) {
+        player_cell = xrs_pixel_to_cell(player.pos.x, player.pos.y);
+        direction = Vector2Subtract(mouse_cell_clicked, player_cell);
+
+        player_cell = Vector2Add(player_cell, direction);
+        player.pos = xrs_cell_to_pixel(player_cell.x, player_cell.y);
+
+        is_walking = false;
+    }
 }
 
 void xrs_update(void) {
-    if (IsKeyPressed(KEY_D)) player.pos.x += 1 * CELL_WIDTH;
+    mouse_pos = GetMousePosition();
+    mouse_world = GetWorldToScreen2D(GetMousePosition(), camera);
 
-    camera.offset = (Vector2) {screen_width / 2, screen_height / 2};
-    camera.rotation = 0.0f;
-    camera.target = player.pos;
-    camera.zoom = 1.00f;
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        mouse_cell_clicked = xrs_pixel_to_cell(mouse_pos.x, mouse_pos.y);
+        is_walking = true;
+    }
 }
 
 void xrs_draw_map(void) {
@@ -75,9 +96,24 @@ void xrs_render(void) {
         ClearBackground(BLACK);
 
         BeginMode2D(camera);
+            Vector2 cell = xrs_pixel_to_cell((int) mouse_pos.x, (int) mouse_pos.y);
+            Vector2 pixel = xrs_cell_to_pixel(cell.x, cell.y);
+
             xrs_draw_map();
             DrawTextureV(player.sprite, player.pos, WHITE);
+
+            DrawText(TextFormat("Tick Time %.2f", timer), 10, 10, 20, BLACK);
+            DrawText(TextFormat("Mouse (%.2f, %.2f)", mouse_pos.x, mouse_pos.y), 100, 100, 20, YELLOW);
+            DrawText(TextFormat("Cell  (%d, %d)", (int) cell.x, (int) cell.y), 100, 125, 20, GOLD);
+            DrawText(TextFormat("World (%.2f, %.2f)", mouse_world.x, mouse_world.y), 100, 150, 20, ORANGE);
+            Vector2 p = xrs_pixel_to_cell(player.pos.x, player.pos.y);
+            DrawText(TextFormat("Player(%.2f, %.2f)", p.x, p.y), 100, 175, 20, PINK);
+            DrawText(TextFormat("Cell Clicked (%.2f, %.2f)", mouse_cell_clicked.x, mouse_cell_clicked.y), 100, 200, 20, RED);
+            DrawText(TextFormat("Direciton (%.2f, %.2f)", direction.x, direction.y), 100, 225, 20, MAROON);
+
+            DrawRectangleLines(pixel.x, pixel.y, CELL_WIDTH, CELL_HEIGHT, PURPLE);
         EndMode2D();
+
     EndDrawing();
 }
 
@@ -88,10 +124,18 @@ void xrs_exit(void) {
     CloseWindow();
 }
 
+float tick_timer = 0.0f;
 int main(void) {
     xrs_init();
 
     while (!WindowShouldClose()) {
+        // Cap xrs_tick_update() to run x amount of times per second.
+        tick_timer += GetFrameTime();
+        if (tick_timer >= 1.0f) {
+            tick_timer = 0.0f;
+            xrs_tick_update();
+        }
+        // xrs_tick_update();
         xrs_update();
         xrs_render();     
     }
